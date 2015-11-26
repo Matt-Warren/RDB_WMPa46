387a4c3d66447e3408007f0b5fea094001a1358e
Matthew Warren <warren.matt@hotmail.com>
2015-11-26 16:21:51 -0500

    Created and set up class

---
+++ ClientServerLibrary/ExcelData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerLibrary
+{
+    public class ExcelData
+    {
+        public int questionNumber;
+        public string questionText;
+        public double avgTime;
+        public double percentCorrect;
+
+        public ExcelData()
+        {
+
+        }
+        public ExcelData(int questionNumber, string questionText, double avgTime, double percentCorrect)
+        {
+            this.questionNumber = questionNumber;
+            this.questionText = questionText;
+            this.avgTime = avgTime;
+            this.percentCorrect = percentCorrect;
+        }
+
+    }
+}
