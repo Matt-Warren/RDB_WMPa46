52f48356824d8d1c055115a7d8fee884ca6df3e2
Matt Warren <warren.matt@hotmail.com>
2015-11-26 16:35:21 -0500

    Added QACombo to library

---
+++ ClientServerLibrary/QACombo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerLibrary
+{
+    [Serializable]
+    public class QACombo
+    {
+
+        public string question { get; set; }
+        public string ans1 { get; set; }
+        public string ans2 { get; set; }
+        public string ans3 { get; set; }
+        public string ans4 { get; set; }
+
+        public int correctAnswer;
+
+        public bool isSet;
+        public QACombo()
+        {
+
+        }
+        public QACombo(string qaString)
+        {
+            string[] temp = qaString.Split('|');
+            this.question = temp[0];
+            this.ans1 = temp[1];
+            this.ans2 = temp[2];
+            this.ans3 = temp[3];
+            this.ans4 = temp[4];
+            this.correctAnswer = Convert.ToInt32(temp[5]);
+            isSet = true;
+        }
+
+        public void SetQACombo(string qaString)
+        {
+            string[] temp = qaString.Split('|');
+            this.question = temp[0];
+            this.ans1 = temp[1];
+            this.ans2 = temp[2];
+            this.ans3 = temp[3];
+            this.ans4 = temp[4];
+            this.correctAnswer = Convert.ToInt32(temp[5]);
+            isSet = true;
+        }
+
+        public bool checkAnswer(int answer)
+        {
+            bool correct = false;
+            if (answer == correctAnswer)
+            {
+                correct = true;
+            }
+            return correct;
+        }
+
+
+    }
+}
