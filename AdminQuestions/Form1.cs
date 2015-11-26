7b7d8ee7c436a0dac5527172482e290d232598ad
Matt Warren <warren.matt@hotmail.com>
2015-11-26 17:55:27 -0500

    Fixed excel stuff, chart left

--- AdminQuestions/Form1.cs
+++ AdminQuestions/Form1.cs
@@ -11,12 +11,16 @@ using ClientServerLibrary;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 using System.IO;
-using ClientServerLibrary;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Reflection;
 
 namespace AdminQuestions
 {
     public partial class mainForm : Form
     {
+        public MemoryStream stream1 = new MemoryStream();
+
         public QACombo currentQA = new QACombo();
         public List<QACombo> questionsList = new List<QACombo>();
         enum checkBoxes
@@ -64,11 +68,11 @@ namespace AdminQuestions
         {
             //save stuff to database
             BinaryFormatter formatter = new BinaryFormatter();
-            using (MemoryStream stream = new MemoryStream())
+            //using (MemoryStream stream = new MemoryStream())
             {
                 try
                 {
-                    formatter.Serialize(stream, questionsList);
+                    formatter.Serialize(stream1, questionsList);
                 }
                 catch (SerializationException se)
                 {
@@ -116,7 +120,8 @@ namespace AdminQuestions
 
         private void lbQuestions_SelectedIndexChanged(object sender, EventArgs e)
         {
-            try {
+            try
+            {
                 string questionNumber = lbQuestions.SelectedItem.ToString();
                 if (questionNumber == "Add a new question...")
                 {
@@ -124,7 +129,7 @@ namespace AdminQuestions
                 }
                 else
                 {
-                    currentQA = questionsList.ElementAt((lbQuestions.SelectedIndex)-1);
+                    currentQA = questionsList.ElementAt((lbQuestions.SelectedIndex) - 1);
                     txtQuestion.Text = currentQA.question;
                     txtAns1.Text = currentQA.ans1;
                     txtAns2.Text = currentQA.ans2;
@@ -152,12 +157,12 @@ namespace AdminQuestions
             {
                 //null string do nothing
             }
-            
+
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if (txtQuestion.Text!="" & txtAns1.Text!="" & txtAns2.Text!="" & txtAns3.Text!="" & txtAns4.Text!="" & (cbAns_1.Checked | cbAns_2.Checked | cbAns_3.Checked | cbAns_4.Checked))
+            if (txtQuestion.Text != "" & txtAns1.Text != "" & txtAns2.Text != "" & txtAns3.Text != "" & txtAns4.Text != "" & (cbAns_1.Checked | cbAns_2.Checked | cbAns_3.Checked | cbAns_4.Checked))
             { //if none of the fields are blank
                 if (!txtQuestion.Text.Contains('|') & !txtAns1.Text.Contains('|') & !txtAns2.Text.Contains('|') & !txtAns3.Text.Contains('|') & !txtAns4.Text.Contains('|'))
                 { //if none of the fields contain a pipe symbol
@@ -168,12 +173,12 @@ namespace AdminQuestions
                         currentQA = new QACombo(txtQuestion.Text + "|" + txtAns1.Text + "|" + txtAns2.Text + "|" + txtAns3.Text + "|" + txtAns4.Text + "|" + (cbAns_1.Checked == true ? 1 : cbAns_2.Checked == true ? 2 : cbAns_3.Checked == true ? 3 : 4));
                         questionsList.Add(currentQA);
                         lbQuestions.Items.Add(currentQA.question);
-                        
+
                     }
                     else
                     {
-                        
-                        currentQA = questionsList.ElementAt(qid-1);
+
+                        currentQA = questionsList.ElementAt(qid - 1);
                         currentQA.question = txtQuestion.Text;
                         currentQA.ans1 = txtAns1.Text;
                         currentQA.ans2 = txtAns2.Text;
@@ -202,7 +207,8 @@ namespace AdminQuestions
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            try {
+            try
+            {
                 string toDelete = lbQuestions.SelectedItem.ToString();
                 if (toDelete != "Add a new question...")
                 {
@@ -212,7 +218,7 @@ namespace AdminQuestions
                     if (result == DialogResult.Yes)
                     {
                         int qid = lbQuestions.SelectedIndex;
-                        questionsList.Remove(questionsList.ElementAt(qid-1));
+                        questionsList.Remove(questionsList.ElementAt(qid - 1));
                         lbQuestions.Items.RemoveAt(qid);
                         clearFields();
 
@@ -231,6 +237,7 @@ namespace AdminQuestions
                 //tostring would have failed
             }
         }
+
         public void clearFields()
         {
             txtQuestion.Text = "";
@@ -258,8 +265,8 @@ namespace AdminQuestions
             }
 
             pLeaderboard.Visible = true;
-            
-            
+
+
         }
 
         private void btnLeaderboardBack_Click(object sender, EventArgs e)
@@ -274,21 +281,133 @@ namespace AdminQuestions
 
         private void btnExcelExport_Click(object sender, EventArgs e)
         {
-            //get list of questions and such from the server
-            MemoryStream stream = new MemoryStream();
+            //get list of questions and such from the server *******************
+            //MemoryStream stream = new MemoryStream();
             BinaryFormatter formatter = new BinaryFormatter();
-            try {
-                stream.Seek(0, SeekOrigin.Begin);
-                List<ExcelData> excelList = (List<ExcelData>)formatter.Deserialize(stream);
 
+            object missing = Type.Missing;
+            Excel.Application oXL = null;
+            Excel.Workbooks oWBs = null;
+            Excel.Workbook oWB = null;
+            Excel.Worksheet oSheet = null;
+            Excel.Range oCells = null;
+            Excel.Range oRng1 = null;
+            Excel.Range oRng2 = null;
 
-
-
-
-            }
-            catch
+            try
             {
+                //stream1.Seek(0, SeekOrigin.Begin);
+                //List<ExcelData> excelList = (List<ExcelData>)formatter.Deserialize(stream1);
+                List<ExcelData> excelList = new List<ExcelData>();
+                excelList.Add(new ExcelData(1, "How does this look?", 13.2, 70.3));
+                excelList.Add(new ExcelData(2, "Q2?", 13.2, 70.3));
+                excelList.Add(new ExcelData(3, "q3?", 13.2, 70.3));
+                excelList.Add(new ExcelData(4, "q44?", 13.2, 70.3));
+                excelList.Add(new ExcelData(5, "q5!?", 13.2, 70.3));
 
+                // Create an instance of Microsoft Excel and make it invisible. 
+                oXL = new Excel.Application();
+                oXL.Visible = false;
+
+                // Create a new Workbook. 
+                oWBs = oXL.Workbooks;
+                oWB = oWBs.Add(missing);
+
+                // Get the active Worksheet and set its name.
+                oSheet = oWB.ActiveSheet as Excel.Worksheet;
+                oSheet.Name = "Question Report";
+
+                // Set the column headers
+                oCells = oSheet.Cells;
+                oCells[1, 1] = "Question Number";
+                oCells[1, 2] = "Question";
+                oCells[1, 3] = "Average Completion Time";
+                oCells[1, 4] = "Percent Correct Answers";
+
+
+                int count = 1;
+                foreach (ExcelData ed in excelList)
+                {
+                    count++;
+                    oCells[count, 1] = ed.questionNumber;
+                    oCells[count, 2] = ed.questionText;
+                    oCells[count, 3] = ed.avgTime;
+                    oCells[count, 4] = ed.percentCorrect;
+                }
+                /*
+                Excel.Range chartRange;
+
+                Excel.ChartObjects xlCharts = (Excel.ChartObjects)xlWorkSheet.ChartObjects(Type.Missing);
+                Excel.ChartObject myChart = (Excel.ChartObject)xlCharts.Add(10, 80, 300, 250);
+                Excel.Chart chartPage = myChart.Chart;
+
+                chartRange = oSheet.get_Range("A1", "d5");
+                chartPage.SetSourceData(chartRange, misValue);
+                chartPage.ChartType = Excel.XlChartType.xlColumnClustered;
+                */
+
+                // Save the workbook as a xlsx file and close it. 
+
+
+                string fileName = Path.GetDirectoryName(
+                    Assembly.GetExecutingAssembly().Location) + "\\QuestionReport.xlsx";
+                oWB.SaveAs(fileName, Excel.XlFileFormat.xlOpenXMLWorkbook,
+                    missing, missing, missing, missing,
+                    Excel.XlSaveAsAccessMode.xlNoChange,
+                    missing, missing, missing, missing, missing);
+                oWB.Close(missing, missing, missing);
+
+                // Quit the Excel application
+                oXL.UserControl = true;
+
+                oXL.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Solution1.AutomateExcel throws the error: {0}",
+                    ex.Message);
+            }
+            finally
+            {
+                // Clean up the unmanaged Excel COM resources by explicitly  
+                // calling Marshal.FinalReleaseComObject on all accessor objects.  
+                // See http://support.microsoft.com/kb/317109. 
+
+                if (oRng2 != null)
+                {
+                    Marshal.FinalReleaseComObject(oRng2);
+                    oRng2 = null;
+                }
+                if (oRng1 != null)
+                {
+                    Marshal.FinalReleaseComObject(oRng1);
+                    oRng1 = null;
+                }
+                if (oCells != null)
+                {
+                    Marshal.FinalReleaseComObject(oCells);
+                    oCells = null;
+                }
+                if (oSheet != null)
+                {
+                    Marshal.FinalReleaseComObject(oSheet);
+                    oSheet = null;
+                }
+                if (oWB != null)
+                {
+                    Marshal.FinalReleaseComObject(oWB);
+                    oWB = null;
+                }
+                if (oWBs != null)
+                {
+                    Marshal.FinalReleaseComObject(oWBs);
+                    oWBs = null;
+                }
+                if (oXL != null)
+                {
+                    Marshal.FinalReleaseComObject(oXL);
+                    oXL = null;
+                }
             }
 
         }
